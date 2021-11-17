a4d8e218fb070d176902aa117371021cb93931f7
Jordan <jrdnvan@gmail.com>
2021-11-17 17:33:10 +1100

    f

---
+++ MyEcsConstruct/tests/unit/test_my_ecs_construct_stack.py
@@ -0,0 +1,19 @@
+# from aws_cdk import (
+#         core,
+#         assertions
+#     )
+
+# from my_ecs_construct.my_ecs_construct_stack import MyEcsConstructStack
+
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in my_ecs_construct/my_ecs_construct_stack.py
+def test_sqs_queue_created():
+#     app = core.App()
+#     stack = MyEcsConstructStack(app, "my-ecs-construct")
+#     template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
+  pass
